3239bd77ff68574452fd41c7094a1118503952b1
Charlie Highum <chighum41@gmail.com>
2021-11-05 03:16:12 -0600

    began writing code for the final steps, creating a password from the inputs and displaying it to the user, mainly just pseudo code for now

--- script.js
+++ script.js
@@ -13,49 +13,121 @@ function writePassword() {
 
 // create generatePassword function
 function generatePassword() {
+  // prompt user for length
   var passwordLength = prompt(
     "How long would you like your password to be? \nMust be between 8 and 128 characters"
   );
-
+  // error handling - is it between 8-128 characters?
   if (passwordLength < 8 || passwordLength > 128) {
     alert("Sorry, that is an invalid input. \nPlease input a number 8 and 128");
     generatePassword();
   } else {
+    function choosecharacters() {
+      // prompt user to confirm all character selections separately
+      // store answers in an array
+      const whatCharacters = [
+        confirm("Would you like lowercase letters in your password?"),
+        confirm("Would you like uppercase letters in your password?"),
+        confirm("Would you like numbers in your password?"),
+        confirm("Would you like special characters in your password?"),
+      ];
+      if (
+        // error handling - did they select at least one option?
+        whatCharacters[0] === false &&
+        whatCharacters[1] === false &&
+        whatCharacters[2] === false &&
+        whatCharacters[3] === false
+      ) {
+        alert("Please select at least one character type.");
+        choosecharacters();
+      } else {
+        function createPassword() {
+          // use inputs to generate password
+          // set the password string length to passwordLength using setAttribute()?
+
+          if (whatCharacters[0]) {
+            // call for random letter from letters array
+          }
+          if (whatCharacters[1]) {
+            // call for random letter from letters.toUpperCase
+          }
+          if (whatCharacters[2]) {
+            var randomNum = Math.floor(Math.random() * 10);
+          }
+          if (whatCharacters[3]) {
+            // call for random special character
+          }
+        }
+        createPassword();
+        // return the password from the generatePassword function either as an alert or in the box
+      }
+    }
     choosecharacters();
   }
 }
-function choosecharacters() {
-  const whatCharacters = [
-    confirm("Would you like lowercase letters in your password?"),
-    confirm("Would you like uppercase letters in your password?"),
-    confirm("Would you like numbers in your password?"),
-    confirm("Would you like special characters in your password?"),
-  ];
-  if (
-    whatCharacters[0] === false &&
-    whatCharacters[1] === false &&
-    whatCharacters[2] === false &&
-    whatCharacters[3] === false
-  ) {
-    alert("Please select at least one character type.");
-    choosecharacters();
-  } else {
-    createPassword();
-  }
-}
 
-// function createPassword() {
-// if whatCharacters
-// }
+// letter array
+const letters = [
+  "a",
+  "b",
+  "c",
+  "d",
+  "e",
+  "f",
+  "g",
+  "h",
+  "i",
+  "j",
+  "k",
+  "l",
+  "m",
+  "n",
+  "o",
+  "p",
+  "q",
+  "r",
+  "s",
+  "t",
+  "u",
+  "v",
+  "x",
+  "y",
+  "z",
+];
 
-// prompt user for length
-// error handling - is it between 8-128 characters?
-
-// prompt user to confirm all character selections separately
-// error handling - did they select at least one option?
-
-// use inputs to generate password
-// return to the password from the generatePassword function
-
+// special characters array
+const specialCharacters = [
+  "!",
+  "@",
+  "#",
+  "$",
+  "%",
+  "^",
+  "&",
+  "*",
+  "?",
+  "'",
+  "(",
+  ")",
+  "+",
+  ",",
+  "-",
+  ".",
+  "/",
+  ":",
+  ";",
+  "<",
+  "=",
+  ">",
+  "[",
+  "]",
+  "_",
+  "`",
+  "{",
+  "|",
+  "}",
+  "~",
+  "~",
+];
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
