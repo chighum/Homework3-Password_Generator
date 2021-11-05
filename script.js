8760c67759e8f7a44e865ed8624682df10c235cb
Charlie Highum <chighum41@gmail.com>
2021-11-05 11:41:02 -0600

    make corresponding changes to error handling to for the previous change from array to object. also added upperCaseLetters array by running a for loop over the lowerCaseLetters array with the toUpperCase() method

--- script.js
+++ script.js
@@ -12,69 +12,69 @@ function writePassword() {
 // Pseudo-code: outline in normal English
 
 // create generatePassword function
-function generatePassword() {
-  // prompt user for length
-  var passwordLength = prompt(
-    "How long would you like your password to be? \nMust be between 8 and 128 characters"
-  );
-  // error handling - is it between 8-128 characters?
-  if (passwordLength < 8 || passwordLength > 128) {
-    alert("Sorry, that is an invalid input. \nPlease input a number 8 and 128");
-    generatePassword();
-  } else {
-    function choosecharacters() {
-      // prompt user to confirm all character selections separately
-      // store answers in an array
-      var whatCharacters = {
-        lowerCase: confirm(
-          "Would you like lowercase letters in your password?"
-        ),
-        upperCase: confirm(
-          "Would you like uppercase letters in your password?"
-        ),
-        numbers: confirm("Would you like numbers in your password?"),
-        specialCharacters: confirm(
-          "Would you like special characters in your password?"
-        ),
-      };
-      if (
-        // error handling - did they select at least one option?
-        whatCharacters.lowerCase === false &&
-        whatCharacters.upperCase === false &&
-        whatCharacters.numbers === false &&
-        whatCharacters.specialCharacters === false
-      ) {
-        alert("Please select at least one character type.");
-        choosecharacters();
-      } else {
-        function createPassword() {
-          // use inputs to generate password
-          // set the password string length to passwordLength using setAttribute()?
+// function generatePassword() {
+//   // prompt user for length
+//   var passwordLength = prompt(
+//     "How long would you like your password to be? \nMust be between 8 and 128 characters"
+//   );
+//   // error handling - is it between 8-128 characters?
+//   if (passwordLength < 8 || passwordLength > 128) {
+//     alert("Sorry, that is an invalid input. \nPlease input a number 8 and 128");
+//     generatePassword();
+//   } else {
+//     function choosecharacters() {
+//       // prompt user to confirm all character selections separately
+//       // store answers in an array
+//       var whatCharacters = {
+//         lowerCase: confirm(
+//           "Would you like lowercase letters in your password?"
+//         ),
+//         upperCase: confirm(
+//           "Would you like uppercase letters in your password?"
+//         ),
+//         numbers: confirm("Would you like numbers in your password?"),
+//         specialCharacters: confirm(
+//           "Would you like special characters in your password?"
+//         ),
+//       };
+//       if (
+//         // error handling - did they select at least one option?
+//         whatCharacters.lowerCase === false &&
+//         whatCharacters.upperCase === false &&
+//         whatCharacters.numbers === false &&
+//         whatCharacters.specialCharacters === false
+//       ) {
+//         alert("Please select at least one character type.");
+//         choosecharacters();
+//       } else {
+//         function createPassword() {
+//           // use inputs to generate password
+//           // set the password string length to passwordLength using setAttribute()?
 
-          if (whatCharacters[0]) {
-            // call for random letter from letters array
-          }
-          if (whatCharacters[1]) {
-            // call for random letter from letters.toUpperCase()
-          }
-          if (whatCharacters[2]) {
-            var randomNum = Math.floor(Math.random() * 10);
-          }
-          if (whatCharacters[3]) {
-            // call for random special character
-          }
-          //
-        }
-        createPassword();
-        // return the password from the generatePassword function either as an alert or in the box
-      }
-    }
-    choosecharacters();
-  }
-}
+//           if (whatCharacters.lowerCase) {
+//             // call for random letter from letters array
+//           }
+//           if (whatCharacters.upperCase) {
+//             // call for random letter from letters.toUpperCase()
+//           }
+//           if (whatCharacters.numbers) {
+//             var randomNum = Math.floor(Math.random() * 10);
+//           }
+//           if (whatCharacters.specialCharacters) {
+//             // call for random special character
+//           }
+//           //
+//         }
+//         createPassword();
+//         // return the password from the generatePassword function either as an alert or in the box
+//       }
+//     }
+//     choosecharacters();
+//   }
+// }
 
 // letter array
-const letters = [
+var lowerCaseLetters = [
   "a",
   "b",
   "c",
@@ -97,13 +97,18 @@ const letters = [
   "t",
   "u",
   "v",
+  "w",
   "x",
   "y",
   "z",
 ];
-
+var upperCaseLetters = lowerCaseLetters;
+for (var i = 0; i < lowerCaseLetters.length; i++) {
+  upperCaseLetters[i] = upperCaseLetters[i].toUpperCase();
+}
+console.log(upperCaseLetters);
 // special characters array
-const specialCharacters = [
+var specialCharacters = [
   "!",
   "@",
   "#",
@@ -133,7 +138,6 @@ const specialCharacters = [
   "{",
   "|",
   "}",
-  "~",
   "~",
 ];
 // Add event listener to generate button
